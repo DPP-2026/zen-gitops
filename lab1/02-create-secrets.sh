30985d2a043d33778388a8d0f4b0e1ae088a0151
DevopsBalasai <chenibalasaivaraprasad@gmail.com>
2026-05-20 17:32:58 +0530

    secrects

--- lab1/02-create-secrets.sh
+++ lab1/02-create-secrets.sh
@@ -7,16 +7,16 @@
 #                  manufacturing, notification, and supplier services.
 # The secret must include both DB_* and SPRING_DATASOURCE_* keys because
 # different services reference different variable names.
-kubectl create secret generic db-credentials \
+kubectl create secret generic db-credential-secret \
   --from-literal=DB_USERNAME=pharmaadmin \
-  --from-literal=DB_PASSWORD=<YOUR_DB_PASSWORD> \
+  --from-literal=DB_PASSWORD=Zen_infra_2026 \
   --from-literal=SPRING_DATASOURCE_USERNAME=pharmaadmin \
-  --from-literal=SPRING_DATASOURCE_PASSWORD=<YOUR_DB_PASSWORD> \
+  --from-literal=SPRING_DATASOURCE_PASSWORD=Zen_infra_2026 \
   -n dev
 
 # jwt-secret — used by auth-service and api-gateway
-kubectl create secret generic jwt-secret \
-  --from-literal=JWT_SECRET=mysupersecretjwtkey256bitslongkey \
+kubectl create secret generic jwt-secret-key \
+  --from-literal=JWT_SECRET=862c8e6b7a6e2f65834fba1ca4ec1e74f0a17907dd4e77663360299e1649bb6b \
   -n dev
 
 # Verify secrets were created
@@ -24,6 +24,6 @@ kubectl get secrets -n dev
 
 # Peek at the values (base64 encoded — NOT encrypted)
 echo "DB_USERNAME:"
-kubectl get secret db-credentials -n dev -o jsonpath='{.data.DB_USERNAME}' | base64 -d && echo
+kubectl get secret db-credential-secret -n dev -o jsonpath='{.data.DB_USERNAME}' | base64 -d && echo
 echo "JWT_SECRET:"
-kubectl get secret jwt-secret -n dev -o jsonpath='{.data.JWT_SECRET}' | base64 -d && echo
+kubectl get secret jwt-secret-key -n dev -o jsonpath='{.data.JWT_SECRET}' | base64 -d && echo
